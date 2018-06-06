ce1d2e5280f8764e08abac86ab92781a8469b9af
unknown <lalance.paul@gmail.com>
2018-06-06 20:02:27 +0200

    maj envoi mail OK

--- mail.php
+++ mail.php
@@ -1,43 +1,20 @@
 <?php
-     // Plusieurs destinataires
-     $to  = 'lalance.paul@mail.com';
 
-     // Sujet
-     $subject = 'Test envoi de mail html';
+ini_set( 'display_errors', 1 );
 
-     // message
-     $message = '
-     <html>
-      <head>
-       <title>Calendrier des anniversaires pour Août</title>
-      </head>
-      <body>
-       <p>Voici les anniversaires à venir au mois d\'Août !</p>
-       <table>
-        <tr>
-         <th>Personne</th><th>Jour</th><th>Mois</th><th>Année</th>
-        </tr>
-        <tr>
-         <td>Josiane</td><td>3</td><td>Août</td><td>1970</td>
-        </tr>
-        <tr>
-         <td>Emma</td><td>26</td><td>Août</td><td>1973</td>
-        </tr>
-       </table>
-      </body>
-     </html>
-     ';
+error_reporting( E_ALL );
 
-     // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
-     $headers  = 'MIME-Version: 1.0' . "\r\n";
-     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$from = "test@votredomaine.com";
 
-//     // En-têtes additionnels
-//     $headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
-//     $headers .= 'From: Anniversaire <anniversaire@example.com>' . "\r\n";
-//     $headers .= 'Cc: anniversaire_archive@example.com' . "\r\n";
-//     $headers .= 'Bcc: anniversaire_verif@example.com' . "\r\n";
+$to = "lalance.paul@gmail.com";
 
-     // Envoi
-     mail($to, $subject, $message, $headers);
+$subject = "Vérification PHP mail";
+
+$message = "PHP mail marche";
+
+$headers = "From:" . $from;
+
+mail($to,$subject,$message, $headers);
+
+echo "L'email a été envoyé.";
 ?>
